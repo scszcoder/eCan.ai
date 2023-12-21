e1478e8daf0e80cefae056c3264dc5242ccc7a9e
scszcoder <1614426112@qq.com>
2023-12-21 13:37:46 -0800

    change data dir reference.

--- bot/envi.py
+++ bot/envi.py
@@ -35,3 +35,35 @@ def getECBotHome():
             ecbhome = ecbhome.replace('\\', '/')
             print("ECBot Home: ", ecbhome)
             return ecbhome
+
+
+def getECBotDataHome():
+    ecb_data_home = ""
+    print(proc_arch)
+#    print(proc_arch64)
+#     if proc_arch == 'x86' and not proc_arch64:
+#         arch_keys = {0}
+#    elif proc_arch == 'x86' or proc_arch == 'amd64':
+    if proc_arch == 'x86' or proc_arch == 'amd64':
+        arch_keys = {winreg.KEY_WOW64_32KEY, winreg.KEY_WOW64_64KEY}
+    else:
+        raise Exception("Unhandled arch: %s" % proc_arch)
+
+    print("arch_keys: ", arch_keys)
+
+    for arch_key in arch_keys:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment", 0, winreg.KEY_READ | arch_key)
+        # print("key: ", key)
+        # print("range: ", winreg.QueryInfoKey(key)[0])
+
+        try:
+            ecb_data_home = winreg.QueryValueEx(key, 'ECBOT_DATA_HOME')[0]
+        except OSError as e:
+            if e.errno == errno.ENOENT:
+                # DisplayName doesn't exist in this skey
+                pass
+        finally:
+            key.Close()
+            ecb_data_home = ecb_data_home.replace('\\', '/')
+            print("ECBot DATA Home: ", ecb_data_home)
+            return ecb_data_home
