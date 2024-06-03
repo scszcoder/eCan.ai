691d2edb19192cd76f4dcf7e3f2280d830c52b26
scszcoder <1614426112@qq.com>
2024-06-03 10:16:09 -0700

    bug fixing for 2 machine configuration.

--- gui/MainGUI.py
+++ gui/MainGUI.py
@@ -4795,7 +4795,7 @@ class MainWindow(QMainWindow):
                 self.workingState = "Idle"
 
             print("running bot works whenever there is some to run....")
-            # await asyncio.sleep(1)
+            await asyncio.sleep(1)
 
 
     #update a vehicle's missions status
