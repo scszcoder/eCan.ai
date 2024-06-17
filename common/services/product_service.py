26c025ed9582844a30507abf46133b85273dfbba
Yu Tang <tangyuncyl@sina.com>
2024-06-18 00:03:36 +0800

    Optimize and simplify code

--- common/services/product_service.py
+++ common/services/product_service.py
@@ -1,7 +1,10 @@
 import json
 
+from sqlalchemy import inspect
+
 from common.db_init import sync_table_columns
 from common.models.product import ProductsModel
+from utils.logger_helper import logger_helper
 
 
 class ProductService:
@@ -16,3 +19,13 @@ class ProductService:
         dict_results = [result.to_dict() for result in results]
         self.main_win.showMsg("fetchall" + json.dumps(dict_results))
         return results
+
+    def describe_table(self):
+        inspector = inspect(ProductsModel)
+        # 打印表结构信息
+        print(f"{ProductsModel.__tablename__} Table column definitions: ")
+        columns = inspector.columns
+        for column in columns:
+            logger_helper.debug(
+                f"Column: {column['name']}, Type: {column['type']}, Nullable: {column['nullable']}, Default: {column['default']}")
+        return columns
