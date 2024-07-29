5470cce8de8f9fecc0f03b7f917ba8b9f008125c
scszcoder <1614426112@qq.com>
2024-07-29 00:38:49 -0700

    added lots of webdriver routines, at this moment, GS scraping almost done, yet to be added 1) multipage result handling 2) set 40 rows per page. 3) data format conversion.

--- bot/adsAPISkill.py
+++ bot/adsAPISkill.py
@@ -1,5 +1,7 @@
 
-
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 API_CONN = "http://local/adspower.net:50360"
 
@@ -17,14 +19,11 @@ ads_api_conns = [
     }
 ]
 
-
-import requests
-
 # Replace with your actual Adspower API key and profile ID
 API_KEY = 'your_adspower_api_key'
 PROFILE_ID = 'your_adspower_profile_id'
 
-def start_adspower_profile(api_key, profile_id):
+def startAdspowerProfile(api_key, profile_id):
     url = f'http://localhost:50325/api/v1/browser/start?user_id={profile_id}'
     headers = {
         'Authorization': f'Bearer {api_key}'
@@ -36,31 +35,20 @@ def start_adspower_profile(api_key, profile_id):
     else:
         raise Exception('Failed to start Adspower profile', response.text)
 
-webdriver_info = start_adspower_profile(API_KEY, PROFILE_ID)
-print('WebDriver Info:', webdriver_info)
+def startADSWebDriver():
+    webdriver_info = startAdspowerProfile(API_KEY, PROFILE_ID)
+    print('WebDriver Info:', webdriver_info)
 
+    webdriver_url = webdriver_info['url']
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('debuggerAddress', f"{webdriver_info['ip']}:{webdriver_info['port']}")
 
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+    # Set up Selenium WebDriver
+    options = Options()
+    options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--disable-gpu")
 
-webdriver_url = webdriver_info['url']
-options = webdriver.ChromeOptions()
-options.add_experimental_option('debuggerAddress', f"{webdriver_info['ip']}:{webdriver_info['port']}")
+    # Connect to the Adspower browser instance
+    driver = webdriver.Remote(command_executor=webdriver_url, desired_capabilities=DesiredCapabilities.CHROME, options=options)
 
-
-# Set up Selenium WebDriver
-options = Options()
-options.add_argument("--headless")  # Run in headless mode
-options.add_argument("--disable-gpu")
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-
-
-
-# Connect to the Adspower browser instance
-driver = webdriver.Remote(command_executor=webdriver_url, desired_capabilities=DesiredCapabilities.CHROME, options=options)
-
-# Now you can control the Adspower browser with Selenium
-driver.get('https://www.google.com')
-print(driver.title)
-driver.quit()
+    return driver
