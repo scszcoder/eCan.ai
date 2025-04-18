991353c3f72719a7450ab0df07d9339196305097
scszcoder <1614426112@qq.com>
2025-04-17 23:20:42 -0700

    try to put all new components together

--- agent/skill.py
+++ agent/skill.py
@@ -12,6 +12,8 @@ from langgraph.prebuilt import create_react_agent
 from typing_extensions import TypedDict
 from langgraph.prebuilt import tools_condition
 import json
+import traceback
+
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
@@ -65,23 +67,107 @@ def build_agent_skills(llm, skill_path=""):
 def build_agent_skills_from_files(llm, skill_path=""):
     return []
 
-def create_rpa_helper_skill(llm):
-    helper_skill = Skill(name="ecbot rpa helper",
-                         description="help fix failures during ecbot RPA runs.")
+def create_rpa_helper_skill(mainwin):
+    try:
+        llm = mainwin.llm
+        helper_skill = Skill(name="ecbot rpa helper",
+                             description="help fix failures during ecbot RPA runs.")
 
-    with MultiServerMCPClient(
-            {
-                "E-Commerce Agents Service": {
-                    # make sure you start your weather server on port 8000
-                    "url": "http://localhost:4668/sse",
-                    "transport": "sse",
+        with MultiServerMCPClient(
+                {
+                    "E-Commerce Agents Service": {
+                        # make sure you start your weather server on port 8000
+                        "url": "http://localhost:4668/sse",
+                        "transport": "sse",
+                    }
                 }
-            }
-    ) as client:
-        # create_react_agent(
-        # 	self.model, tools=self.tools, checkpointer=memory, prompt=self.SYSTEM_INSTRUCTION,
-        # 	response_format=ResponseFormat
-        # )
-        mcp_agent = create_react_agent(llm, client.get_tools())
-        helper_skill.set_runnable(mcp_agent)
+        ) as client:
+            # create_react_agent(
+            # 	self.model, tools=self.tools, checkpointer=memory, prompt=self.SYSTEM_INSTRUCTION,
+            # 	response_format=ResponseFormat
+            # )
+            mcp_agent = create_react_agent(llm, client.get_tools())
+            helper_skill.set_runnable(mcp_agent)
+    except Exception as e:
+        # Get the traceback information
+        traceback_info = traceback.extract_tb(e.__traceback__)
+        # Extract the file name and line number from the last entry in the traceback
+        if traceback_info:
+            ex_stat = "ErrorCreateRPAHelperSkill:" + traceback.format_exc() + " " + str(e)
+        else:
+            ex_stat = "ErrorCreateRPAHelperSkill: traceback information not available:" + str(e)
+        mainwin.showMsg(ex_stat)
+        return None
+
+    return helper_skill
+
+
+def create_rpa_operator_skill(mainwin):
+    try:
+        llm = mainwin.llm
+        helper_skill = Skill(name="ecbot rpa operator",
+                             description="help run ecbot RPA works.")
+
+        with MultiServerMCPClient(
+                {
+                    "E-Commerce Agents Service": {
+                        # make sure you start your weather server on port 8000
+                        "url": "http://localhost:4668/sse",
+                        "transport": "sse",
+                    }
+                }
+        ) as client:
+            # create_react_agent(
+            # 	self.model, tools=self.tools, checkpointer=memory, prompt=self.SYSTEM_INSTRUCTION,
+            # 	response_format=ResponseFormat
+            # )
+            mcp_agent = create_react_agent(llm, client.get_tools())
+            helper_skill.set_runnable(mcp_agent)
+    except Exception as e:
+        # Get the traceback information
+        traceback_info = traceback.extract_tb(e.__traceback__)
+        # Extract the file name and line number from the last entry in the traceback
+        if traceback_info:
+            ex_stat = "ErrorCreateRPAOperatorSkill:" + traceback.format_exc() + " " + str(e)
+        else:
+            ex_stat = "ErrorCreateRPAOperatorSkill: traceback information not available:" + str(e)
+        mainwin.showMsg(ex_stat)
+        return None
+
+    return helper_skill
+
+
+
+def create_rpa_supervisor_skill(mainwin):
+    try:
+        llm = mainwin.llm
+        helper_skill = Skill(name="ecbot rpa supervisor",
+                             description="Supervise ecbot RPA operators.")
+
+        with MultiServerMCPClient(
+                {
+                    "E-Commerce Agents Service": {
+                        # make sure you start your weather server on port 8000
+                        "url": "http://localhost:4668/sse",
+                        "transport": "sse",
+                    }
+                }
+        ) as client:
+            # create_react_agent(
+            # 	self.model, tools=self.tools, checkpointer=memory, prompt=self.SYSTEM_INSTRUCTION,
+            # 	response_format=ResponseFormat
+            # )
+            mcp_agent = create_react_agent(llm, client.get_tools())
+            helper_skill.set_runnable(mcp_agent)
+    except Exception as e:
+        # Get the traceback information
+        traceback_info = traceback.extract_tb(e.__traceback__)
+        # Extract the file name and line number from the last entry in the traceback
+        if traceback_info:
+            ex_stat = "ErrorCreateRPASupervisorSkill:" + traceback.format_exc() + " " + str(e)
+        else:
+            ex_stat = "ErrorCreateRPASupervisorSkill: traceback information not available:" + str(e)
+        mainwin.showMsg(ex_stat)
+        return None
+
     return helper_skill
