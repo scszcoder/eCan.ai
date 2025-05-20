3da6118773b73786ac5cc3681ab64f0dca35b900
liu qiang <249511118@qq.com>
2025-05-20 11:39:03 +0800

    clean code

--- gui_v2/src/App.tsx
+++ gui_v2/src/App.tsx
@@ -1,31 +1,46 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ConfigProvider } from 'antd';
+import { ConfigProvider, theme } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import enUS from 'antd/locale/en_US';
 import { useTranslation, I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import { routes } from './routes';
+import { ThemeProvider, useTheme } from './contexts/ThemeContext';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
   const { i18n: i18nInstance } = useTranslation();
+  const { theme: currentTheme } = useTheme();
   const currentLocale = i18nInstance.language === 'zh-CN' ? zhCN : enUS;
 
   return (
+    <ConfigProvider
+      locale={currentLocale}
+      theme={{
+        algorithm: currentTheme === 'dark' ? theme.darkAlgorithm : theme.defaultAlgorithm,
+      }}
+    >
+      <Router>
+        <Routes>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.element}
+            />
+          ))}
+        </Routes>
+      </Router>
+    </ConfigProvider>
+  );
+};
+
+const App: React.FC = () => {
+  return (
     <I18nextProvider i18n={i18n}>
-      <ConfigProvider locale={currentLocale}>
-        <Router>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Routes>
-        </Router>
-      </ConfigProvider>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
     </I18nextProvider>
   );
 };
