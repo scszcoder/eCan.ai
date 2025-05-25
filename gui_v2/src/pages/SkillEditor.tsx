f407793de85c338a0b72d61ee9ab22dcaf9efa65
liu qiang <249511118@qq.com>
2025-05-25 13:01:36 +0800

    add flowgram

--- gui_v2/src/pages/SkillEditor.tsx
+++ gui_v2/src/pages/SkillEditor.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, Typography } from 'antd';
 import { logger } from '../utils/logger';
+import { Editor } from '../modules/skill-editor/editor';
 
 const { Title } = Typography;
 
@@ -8,6 +9,7 @@ const SkillEditor: React.FC = () => {
     logger.debug('SkillEditor component rendered');
 
     return (
+        // <Editor></Editor>
         <Card>
             <Title level={2}>Skill Editor</Title>
             <p>Skill editor content will be implemented here.</p>
