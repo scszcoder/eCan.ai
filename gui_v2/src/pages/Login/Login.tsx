f99c5f287538ac820e4f00164519c2f57bf6f75d
liu qiang <249511118@qq.com>
2025-06-16 18:57:56 +0800

    fix bug  of   i18n

--- gui_v2/src/pages/Login/Login.tsx
+++ gui_v2/src/pages/Login/Login.tsx
@@ -321,7 +321,7 @@ const Login: React.FC = () => {
 							</div>
 
 								{/* Debug Login Button */}
-								<Row style={{ marginTop: 16 }} hidden={true}>
+								<Row style={{ marginTop: 16 }} hidden={false}>
 									<Col span={24}>
 										<Button
 											type="dashed"
