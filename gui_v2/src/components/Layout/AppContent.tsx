a60cb2b766b324851a9fc8b55238631630b39e42
liu qiang <249511118@qq.com>
2025-06-19 18:58:29 +0800

    change main layout

---
+++ gui_v2/src/components/Layout/AppContent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Content } from 'antd/es/layout/layout';
+import styled from '@emotion/styled';
+
+const StyledContent = styled(Content)`
+    margin: 24px 16px;
+    padding: 24px;
+    background: #fff;
+    min-height: 280px;
+    border-radius: 8px;
+`;
+
+const AppContent: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+    <StyledContent>{children}</StyledContent>
+);
+
+export default AppContent; 
