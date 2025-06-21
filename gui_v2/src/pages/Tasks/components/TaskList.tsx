e21802c95d04a039cd1a998f22c16378d837afd6
liu qiang <249511118@qq.com>
2025-06-22 03:47:08 +0800

    clean tasks

--- gui_v2/src/pages/Tasks/components/TaskList.tsx
+++ gui_v2/src/pages/Tasks/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { Avatar, List, Space, Tag, Typography } from 'antd';
+import { Avatar, List, Space, Tag, Typography, theme } from 'antd';
 import { CodeOutlined, OrderedListOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
@@ -7,13 +7,19 @@ import { Task } from '../types';
 
 const { Text } = Typography;
 
-const TaskItem = styled.div<{ $isActive: boolean }>`
+const TaskItem = styled.div<{
+  $isActive: boolean;
+  $activeBg: string;
+  $hoverBg: string;
+  $borderRadius: number;
+}>`
   padding: 12px;
-  border-bottom: 1px solid #eee;
+  margin: 4px 8px;
+  border-radius: ${(props) => props.$borderRadius}px;
   cursor: pointer;
-  background-color: ${(props) => (props.$isActive ? '#e6f7ff' : 'transparent')};
+  background-color: ${(props) => (props.$isActive ? props.$activeBg : 'transparent')};
   &:hover {
-    background-color: #f5f5f5;
+    background-color: ${(props) => props.$hoverBg};
   }
 `;
 
@@ -31,24 +37,31 @@ interface TaskListProps {
 
 export const TaskList: React.FC<TaskListProps> = ({ tasks, onSelectItem, isSelected }) => {
   const { t } = useTranslation();
+  const { token } = theme.useToken();
 
   return (
     <List
       dataSource={tasks}
       renderItem={(task) => (
-        <TaskItem onClick={() => onSelectItem(task)} $isActive={isSelected(task)}>
+        <TaskItem
+          onClick={() => onSelectItem(task)}
+          $isActive={isSelected(task)}
+          $activeBg={token.colorPrimaryBg}
+          $hoverBg={token.colorBgTextHover}
+          $borderRadius={token.borderRadiusLG}
+        >
           <Space direction="vertical" style={{ width: '100%' }}>
             <Space>
               <Avatar icon={<OrderedListOutlined />} />
               <Text strong>{task.skill}</Text>
             </Space>
             <Space>
-              <Tag color={getStatusColor(task.state.top)}>{task.state.top}</Tag>
+              <Tag color={getStatusColor(task.state.top)}>{t('pages.tasks.states.' + task.state.top, task.state.top)}</Tag>
               <Tag icon={<CodeOutlined />}>
-                {t('pages.tasks.trigger')}: {task.trigger}
+                {t('pages.tasks.trigger', { trigger: task.trigger })}
               </Tag>
               <Tag>
-                {t('pages.tasks.priority')}: {task.priority}
+                {t('pages.tasks.priority', { priority: task.priority })}
               </Tag>
             </Space>
           </Space>
