74b29e55a1aeeef7c5da989aec7e72254de6d419
scszcoder <1614426112@qq.com>
2025-06-21 22:20:38 -0700

    add more store for agent and skill

---
+++ gui_v2/src/stores/agentStore.ts
@@ -0,0 +1,12 @@
+// stores/userStore.ts
+import { create } from 'zustand';
+
+interface AgentState {
+  agentname: string | null;
+  setAgentname: (agentname: string) => void;
+}
+
+export const useAgentStore = create<AgentState>((set) => ({
+  agentname: null,
+  setAgentname: (agentname) => set({ agentname }),
+}));
