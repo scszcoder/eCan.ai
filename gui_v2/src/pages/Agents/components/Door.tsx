9f605502a9f27100f1cdf8284483f35ff736715d
liu qiang <249511118@qq.com>
2025-07-02 03:40:26 +0800

    clean code

--- gui_v2/src/pages/Agents/components/Door.tsx
+++ gui_v2/src/pages/Agents/components/Door.tsx
@@ -1,28 +1,14 @@
 import React, { useState } from 'react';
 import doorClosedImg from '@/assets/icons1_door_256.png';
 import doorOpenImg from '@/assets/icons3_door_256.png';
-import agentGifs from '@/assets/gifs';
 import './Door.css';
 
 interface DoorProps {
   name: string;
 }
 
-const primary = 'var(--ant-primary-color, #1677ff)';
-const primaryDark = 'var(--ant-primary-6, #0958d9)';
-const gold = '#E2B13C';
-const goldDark = '#B07B3B';
-const textColor = 'var(--ant-text-color, #222)';
-const borderRadius = 24;
-
-function getRandomGif() {
-  const idx = Math.floor(Math.random() * agentGifs.length);
-  return agentGifs[idx];
-}
-
 const Door: React.FC<DoorProps> = ({ name }) => {
   const [hovered, setHovered] = useState(false);
-  const [gifUrl] = useState(getRandomGif());
 
   return (
     <div
