f5506f795f2f178966a71684e2afe281366d13ef
liu qiang <249511118@qq.com>
2025-07-21 17:54:22 +0800

    fix bug of chat

--- gui_v2/src/pages/Chat/utils/displayMsg.ts
+++ gui_v2/src/pages/Chat/utils/displayMsg.ts
@@ -4,17 +4,17 @@ export function getDisplayMsg(content: any, t: any): string {
     if (c && typeof c === 'object' && c !== null && 'type' in c) {
         switch (c.type) {
             case 'text': return c.text || t('pages.chat.noMessages');
-            case 'form': return t('pages.chat.form');
-            case 'image_url': return t('pages.chat.image');
-            case 'file_url': return t('pages.chat.file');
-            case 'code': return t('pages.chat.code');
-            case 'system': return t('pages.chat.system');
-            case 'notification': return t('pages.chat.notification');
-            case 'card': return t('pages.chat.card');
-            case 'markdown': return t('pages.chat.markdown');
-            case 'table': return t('pages.chat.table');
-            default: return t('pages.chat.unknownType');
+            case 'form': return t('pages.chat.lastMsg.text');
+            case 'image_url': return t('pages.chat.lastMsg.image');
+            case 'file_url': return t('pages.chat.lastMsg.file');
+            case 'code': return t('pages.chat.lastMsg.code');
+            case 'system': return t('pages.chat.lastMsg.system');
+            case 'notification': return t('pages.chat.lastMsg.notification');
+            case 'card': return t('pages.chat.lastMsg.card');
+            case 'markdown': return t('pages.chat.lastMsg.markdown');
+            case 'table': return t('pages.chat.lastMsg.table');
+            default: return t('pages.chat.lastMsg.unknownType');
         }
     }
-    return t('pages.chat.unknownType');
+    return t('pages.chat.lastMsg.unknownType');
 } 
