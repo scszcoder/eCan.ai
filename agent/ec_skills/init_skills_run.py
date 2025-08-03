b5fda087a1097a82b6e3f588a1c45145132aef0d
scszcoder <1614426112@qq.com>
2025-08-03 09:54:43 -0700

    in process of getting chat notification to work.

--- agent/ec_skills/init_skills_run.py
+++ agent/ec_skills/init_skills_run.py
@@ -2,9 +2,12 @@ from agent.ec_skills.search_1688.init_search_1688_skill import *
 from agent.ec_skills.search_digi_key.init_search_digi_key_skill import *
 from agent.ec_skills.search_1688.init_search_1688_chatter_skill import *
 # from agent.ec_skills.search_digi_key.init_search_digi_key_chatter_skill import *
+from agent.ec_skills.search_parts.init_search_parts_skill import *
+from agent.ec_skills.search_parts.init_search_parts_chatter_skill import *
 from agent.ec_skills.ecbot_rpa.init_ecbot_rpa_skill import *
 from agent.ec_skills.ecbot_rpa.init_ecbot_rpa_chatter_skill import *
 from agent.ec_skills.my_twin.init_my_twin_chatter_skill import *
+
 SKILL_INIT_TABLE = {
     "chatter for my digital twin": init_my_twin_chatter_skill,
     "ecbot rpa helper": init_ecbot_rpa_helper_skill,
@@ -14,8 +17,10 @@ SKILL_INIT_TABLE = {
     "ecbot rpa supervisor task scheduling": init_ecbot_rpa_superviser_skill,
     "chatter for ecbot rpa supervisor": init_ecbot_rpa_superviser_chatter_skill,
     "meca search 1688 web site": init_search_1688_skill,
+    "chatter for meca search 1688 web site": init_search_1688_chatter_skill,
     "meca search digi-key web site": init_search_digi_key_skill,
-    "chatter for meca search 1688 web site": init_search_1688_chatter_skill
+    "chatter for ecan.ai search parts and components web site": init_search_parts_chatter_skill,
+    "ecan.ai search parts and components web site": init_search_parts_skill
 }
 
 def init_skills_run(skillName, agent, msg=None):
--- agent/tasks.py
+++ agent/tasks.py
@@ -473,7 +473,7 @@ def time_to_run(agent):
 
     for task in agent.tasks:
         # 🚨 Skip tasks without schedule or non-time-based tasks
-        if not task.schedule or task.schedule.repeat_type == Repeat_Types.NONE:
+        if not task.schedule or task.schedule.repeat_type == Repeat_Types.NONE or task.trigger != "schedule":
             continue
 
         last_runtime, next_runtime = get_runtime_bounds(task.schedule)
@@ -650,20 +650,43 @@ class TaskRunner(Generic[Context]):
             task.status.message.parts.append(Part(type="text", text=str(injected_state)))
         await self.resume_task(task_id)
 
-    def sendChatToGUI(self, sender_agent, chatId, msg):
-        print("sendChatToGUI::", msg)
+    def sendChatMessageToGUI(self, sender_agent, chatId, msg):
+        print("sendChatMessageToGUI::", msg)
         try:
             msg_data = {
-                "from": "",
-                "to": "",
-                "message": msg,
-                "time": ""
+                "role": 'agent',
+                "id": "string",
+                "createAt": int(time.time() * 1000),
+                "content": msg,         # string | Content | Content[] 支持字符串、单个Content对象或Content数组
+                "status": "sent"        # 使用枚举类型
+
             }
+
             resp = self.agent.mainwin.top_gui.receive_new_chat_message(sender_agent, chatId,msg_data)
             # ipc_api.update_chats([msg])
         except Exception as e:
             ex_stat = "ErrorSendChat2GUI:" + traceback.format_exc() + " " + str(e)
             print(f"{ex_stat}")
+
+    def sendChatDataToGUI(self, sender_agent, chatId, chatData):
+        print("sendChatDataToGUI::", chatData)
+        try:
+            msg_data = {
+                "role": 'agent',
+                "id": "string",
+                "createAt": int(time.time() * 1000),
+                "content": chatData,         # string | Content | Content[] 支持字符串、单个Content对象或Content数组
+                "status": "sent"        # 使用枚举类型
+
+            }
+
+            resp = self.agent.mainwin.top_gui.receive_new_chat_notification(sender_agent, chatId,msg_data)
+            # ipc_api.update_chats([msg])
+        except Exception as e:
+            ex_stat = "ErrorSendChat2GUI:" + traceback.format_exc() + " " + str(e)
+            print(f"{ex_stat}")
+
+
 
     def find_chatter_tasks(self):
         # for now, for the simplicity just find the task that's not scheduled.
@@ -706,12 +729,14 @@ class TaskRunner(Generic[Context]):
         while not self._stop_event.is_set():
             try:
                 logger.trace("checking scheduled task...." + self.agent.card.name)
-
+                print("checking agent scheduled task...." + self.agent.card.name)
                 # if nothing on queue, do a quick check if any vehicle needs a ping-pong check
                 logger.trace("Checking schedule.....")
                 task2run = time_to_run(self.agent)
+                print("task2run:", task2run)
                 logger.trace(f"len task2run, {task2run}, {self.agent.card.name}")
                 if task2run:
+                    print("setting up scheduled task to run", task2run.name)
                     logger.debug("scheduled task2run skill name" + task2run.skill.name)
                     task2run.metadata["state"] = init_skills_run(task2run.skill.name, self.agent)
 
@@ -724,6 +749,7 @@ class TaskRunner(Generic[Context]):
                     else:
                         self.agent.a2a_server.task_manager.set_exception(task2run.id, RuntimeError("Task failed"))
                 else:
+                    print("schedule task not reached scheduled time yet....")
                     logger.debug("nothing 2 run")
 
             except Exception as e:
@@ -768,7 +794,7 @@ class TaskRunner(Generic[Context]):
                                     prompt = interrupt_obj.value["prompt_to_human"]
                                     # now return this prompt to GUI to display
                                     chatId = msg.params.metadata['chatId']
-                                    self.sendChatToGUI(chatId, prompt)
+                                    self.sendChatMessageToGUI(chatId, prompt)
 
                             task_id = msg.params.id
                             self.agent.a2a_server.task_manager.resolve_waiter(task_id, response)
@@ -802,7 +828,7 @@ class TaskRunner(Generic[Context]):
         while chatNotDone:
             try:
                 logger.trace("checking chat queue...." + self.agent.card.name)
-                # chatResponse = self.sendChatToGUI("User (q/Q to quit): ")
+                # chatResponse = self.sendChatMessageToGUI("User (q/Q to quit): ")
                 thread_config = {"configurable": {"thread_id": uuid.uuid4()}}
                 if not self.chat_msg_queue.empty():
                     try:
@@ -840,7 +866,7 @@ class TaskRunner(Generic[Context]):
                                     chatId = msg.params.metadata['chatId']
                                     task_id = msg.params.metadata['msgId']
                                     print("chatId in the message", chatId)
-                                    self.sendChatToGUI(self.agent, chatId, prompt)
+                                    self.sendChatMessageToGUI(self.agent, chatId, prompt)
                                     print("prompt sent to GUI<<<<<<<<<<<")
 
                                 if not isinstance(msg, dict):
@@ -880,7 +906,7 @@ class TaskRunner(Generic[Context]):
                                     chatId = msg.params.metadata['chatId']
                                     task_id = msg.params.metadata['msgId']
                                     print("chatId in the message", chatId)
-                                    self.sendChatToGUI(self.agent, chatId, prompt)
+                                    self.sendChatMessageToGUI(self.agent, chatId, prompt)
                                     print("prompt sent to GUI<<<<<<<<<<<")
 
                                 if not isinstance(msg, dict):
