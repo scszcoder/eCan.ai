9ee7e8c852a35a8929b5205ff6fdcd6648109323
scszcoder <1614426112@qq.com>
2025-08-12 18:28:06 -0700

    update self test agent+skill+task

--- agent/ec_skills/self_test/init_self_test_skill.py
+++ agent/ec_skills/self_test/init_self_test_skill.py
@@ -1,5 +1,5 @@
-def init_search_parts_skill(agent, msg=None, file_names=[]):
-    print("init_search_parts_skill", type(msg), msg)  # msg.params.message[0].text
+def init_self_test_skill(agent, msg=None, file_names=[]):
+    print("init_self_test_skill", type(msg), msg)  # msg.params.message[0].text
 
     attachments = []
     msg_txt = ""
