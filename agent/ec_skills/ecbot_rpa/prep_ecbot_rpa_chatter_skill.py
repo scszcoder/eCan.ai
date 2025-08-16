cdd496c7315b2687487a4c56cea1845185bd6718
scszcoder <1614426112@qq.com>
2025-08-16 16:37:25 -0700

    big merge and add login in by google ui， skills change for search parts chatter.

--- agent/ec_skills/ecbot_rpa/prep_ecbot_rpa_chatter_skill.py
+++ agent/ec_skills/ecbot_rpa/prep_ecbot_rpa_chatter_skill.py
@@ -1,24 +1,23 @@
 
 
 
-def init_ecbot_rpa_helper_skill(agent, msg=''):
-    msg_txt = msg
+def prep_ecbot_rpa_helper_chatter_skill(agent, msg):
+    msg = ""
     return {
         "messages": [agent.card.id, msg],
         "attributes": {}
     }
 
-def init_ecbot_rpa_operator_skill(agent, msg=''):
-    msg_txt = msg
+def prep_ecbot_rpa_operator_chatter_skill(agent, msg):
+    msg_txt = msg['params']['content']
     return {
         "messages": [agent.card.id, msg_txt],
         "attributes": {}
     }
 
-def init_ecbot_rpa_superviser_skill(agent, msg=''):
-    msg_txt = msg
+def prep_ecbot_rpa_superviser_chatter_skill(agent, msg):
+    msg_txt = msg['params']['content']
     return {
         "messages": [agent.card.id, msg_txt],
         "attributes": {}
     }
-
