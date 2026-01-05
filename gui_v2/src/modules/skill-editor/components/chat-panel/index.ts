1bf0a8cb3d180eeef8b0fcf3e0c6d7f90259302e
scszcoder <1614426112@qq.com>
2026-01-04 21:39:01 -0800

    continue to beef up gui for chat based skill editor.

--- gui_v2/src/modules/skill-editor/components/chat-panel/index.ts
+++ gui_v2/src/modules/skill-editor/components/chat-panel/index.ts
@@ -1,3 +1,4 @@
 export { ChatPanel } from './ChatPanel';
 export { FloatingToggleButton } from './FloatingToggleButton';
 export { ResizableDivider } from './ResizableDivider';
+export { CuteRobotIcon } from './CuteRobotIcon';
